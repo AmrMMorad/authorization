92c35c084025c3f687319082fc39915c4eec55ee
AmrMMorad <amr.morad@bibalex.org>
2018-03-17 23:08:53 +0200

    JWT

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 require './lib/sinatra/authorization_helper'
 require 'bcrypt'
+require 'json'
+require 'jwt'
 
 # password is hashed in the database. we cannot put it as plain text
 class AuthorizationController < Sinatra::Base
@@ -8,12 +10,27 @@ class AuthorizationController < Sinatra::Base
 
 	post '/login' do
 		user = User.find(email: params[:email])
-		puts user.email
 	    if user && test_password(params[:password], user.password)
-	    	status 200
-	    	puts "success"
-	    else
-	    	puts "NNNNN"
+			content_type :json
+			{ token: token(user.email) }.to_json
+		else
+	    	halt 401
 		end
 	end
+
+	def token email
+		JWT.encode payload(email), 'HS256'
+	end
+
+	def payload email
+		{
+			exp: Time.now.to_i + 60 * 60,
+			iat: Time.now.to_i,
+			scopes: ['authorize'],
+			user: {
+				email: email
+			}
+		}
+	end
+
 end
